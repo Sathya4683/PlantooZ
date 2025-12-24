931caf3d7932c4b420946ca6f7cfa3565f8cce96
Sathya Narayanan <sathyanarayanan2548@gmail.com>
2025-12-24 20:00:43 +0530

    swagger docs: just refer / update in this

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,12 @@ const testRoutes = require("./routes/testRoutes");
 const itemRoutes = require("./routes/itemRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const morgan = require("morgan");
+const { swaggerUi, swaggerDocument } = require("./swagger");
 
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
-
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Mount /users routes
 app.use("/users", testRoutes);
 app.use("/items", itemRoutes);
