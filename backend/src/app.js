0fcab7f74ce691a728975e4d00f227b8a947b8af
Pranav-B-0071 <pranavbalamurugan1318@gmail.com>
2025-12-26 11:23:28 +0530

    "small change in app.js"

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ import testRoutes from "./routes/testRoutes.js";
 import itemRoutes from "./routes/itemRoutes.js";
 import chatRoutes from "./routes/chatRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+import streakRoutes from "./routes/streakRoutes.js";
 import { swaggerUi, swaggerDocument } from "./swagger.js";
 
 const app = express();
@@ -13,7 +14,7 @@ const app = express();
 /* ---------------- Middleware ---------------- */
 
 app.use(morgan("dev"));
-app.use(cors());               // ✅ added
+app.use(cors());
 app.use(express.json());
 
 /* ---------------- Docs ---------------- */
@@ -26,6 +27,7 @@ app.use("/users", testRoutes);
 app.use("/items", itemRoutes);
 app.use("/chat", chatRoutes);
 app.use("/posts", postRoutes);
+app.use("/streak", streakRoutes); 
 
 /* ---------------- Export ---------------- */
 
