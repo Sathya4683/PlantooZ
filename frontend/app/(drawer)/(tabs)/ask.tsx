d7dc9c36305f060878093b7580efd20f1596daa6
Sathya Narayanan <sathyanarayanan2548@gmail.com>
2025-12-26 21:33:01 +0530

    adding assets

--- frontend/app/(drawer)/(tabs)/ask.tsx
+++ frontend/app/(drawer)/(tabs)/ask.tsx
@@ -50,7 +50,7 @@ export default function Ask() {
 
     try {
       const res = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${API_KEY}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
@@ -117,7 +117,7 @@ ${prompt}
 
       /* STEP 1: detect plant type */
       const detectRes = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${API_KEY}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
